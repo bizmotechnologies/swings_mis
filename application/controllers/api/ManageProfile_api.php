5c9d4c5c3e7dee7e8561235850b64e55e77e6d53
being-samrat <mundesamrat@gmail.com>
2017-11-22 01:03:26 +0530

    commit by sam: profile saving complete

---
+++ application/controllers/api/ManageProfile_api.php
@@ -0,0 +1,61 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+require(APPPATH.'/libraries/REST_Controller.php');
+class ManageProfile_api extends REST_Controller
+{
+	public function __construct(){
+		parent::__construct();
+		$this->load->model('inventory_model/ManageProfile_model');
+		$this->load->model('sales_model/Enquiry_model');
+
+	}
+
+
+	// -----------------------GET ALL PRODUCTS API----------------------//
+	//-------------------------------------------------------------//
+	public function all_profile_get(){
+
+		$result = $this->ManageProfile_model->getAll_profile();
+		return $this->response($result);			
+	}
+	//---------------------GET ALL PRODUCTS END------------------------------//
+
+	// -----------------------GET PARTICULAR PRODUCTS API----------------------//
+	//-------------------------------------------------------------//
+	public function profileDetails_get(){
+		$profile_id=$_GET['profile_id'];
+		$result = $this->ManageProfile_model->getProfile_details($profile_id);
+		return $this->response($result);			
+	}
+	//---------------------GET PARTICULAR PRODUCTS END------------------------------//	
+
+
+// -----------------------ADD PRODUCT IN QUOTATION API----------------------//
+	//-------------------------------------------------------------//
+	public function save_Profile_post(){
+		$data=$_POST;		
+		$response = $this->ManageProfile_model->save_Profile($data);
+		return $this->response($response);			
+	}
+	//---------------------ADD PRODUCT IN QUOTATION END------------------------------//
+
+
+// -----------------------GET DELETE PRODUCT RECORD API----------------------//
+	//-------------------------------------------------------------//
+	public function DeleteRecord_get(){
+		$data = $_GET;
+		$response = $this->ManageProfile_model->DeleteRecord($data);     
+		return $this->response($response);			
+	}
+	//---------------------GET PRODUCT RECORDS END------------------------------//
+// -----------------------GET UPDATE PRODUCT RECORD API----------------------//
+	//-------------------------------------------------------------//
+	public function UpdateProfileRecord_post(){
+		$data = $_POST;
+		$response = $this->ManageProfile_model->UpdateProfileRecord($data);     
+		return $this->response($response);			
+	}
+	//---------------------GET PRODUCT UPDATE RECORDS END------------------------------//
+
+}
--- application/controllers/inventory/Manage_profiles.php
+++ application/controllers/inventory/Manage_profiles.php
@@ -49,48 +49,76 @@ public function addProfile() {
 
   $material_Arr=array();  //material_image array
   $allowed_types=['gif','jpg','png','jpeg','JPG','GIF','JPEG','PNG'];
+  $extension_profile='';
   for($i = 0; $i < count($_FILES['material_image']['name']); $i++){
 
-    $extension = pathinfo($_FILES['material_image']['name'][$i], PATHINFO_EXTENSION); //get file extension 
+    $extension_material = pathinfo($_FILES['material_image']['name'][$i], PATHINFO_EXTENSION); //get material image file extension 
+    $extension_profile = pathinfo($_FILES['profile_image']['name'], PATHINFO_EXTENSION); //get profile image file extension 
 
     //image validating---------------------------//
     //check whether image size is less than 1 mb or not
-    if($_FILES['material_image']['size'][$i] > 1048576){
-      echo '<label class="w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> Image size for material '.$material_name[$i].' exceeds size limit of 1MB. Upload image having size less than 1MB</label>';
+    if($_FILES['material_image']['size'][$i] > 1048576){  //for material images
+      echo '<label class="w3-small w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> Image size for material '.$material_name[$i].' exceeds size limit of 1MB. Upload image having size less than 1MB</label>';
+      die();
+    }
+    if($_FILES['profile_image']['size'][$i] > 1048576){ //for profile image
+      echo '<label class="w3-small w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> Profile Image size exceeds size limit of 1MB. Upload image having size less than 1MB</label>';
       die();
     }
 
     //check file is an image or not by checking extensions
-    if(!in_array($extension, $allowed_types)){
-      echo '<label class="w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> File uploading for material '.$material_name[$i].' is not an image file. Upload image having type gif, jpg, jpeg OR png</label>';
+    if(!in_array($extension_material, $allowed_types)){  //for material images
+      echo '<label class="w3-small w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> File uploading for material '.$material_name[$i].' is not an image file. Upload image having type gif, jpg, jpeg OR png</label>';
+      die();
+    }
+    if(!in_array($extension_profile, $allowed_types)){  //for profile image
+      echo '<label class="w3-small w3-label w3-text-red"><i class="fa fa-warning w3-xxlarge"></i> File uploading for profile '.$profile_name.' is not an image file. Upload image having type gif, jpg, jpeg OR png</label>';
       die();
     }
   }
   //validating image ends---------------------------//
+
+  //uploading profile image-------------------------------//
+  $profileImg_path='';
+
+  if(!empty($_FILES['profile_image']['name'])){
+      $_FILES['profile_image']['name']=$profile_name.'.'.$extension_profile;
+
+      $config['upload_path']  = 'images/desktop/';
+      $config['allowed_types']= 'gif|jpg|png|jpeg';
+      $config['overwrite']   = TRUE;
+
+    $this->load->library('upload', $config);
+    $this->upload->initialize($config);
+    if($this->upload->do_upload('profile_image'))
+    {
+      $profileImg_path='images/desktop/'.$profile_name.'.'.$extension_profile;
+    }
+
+  }
+  //uploading profile image ends-------------------------//
 
   $imagePath ='';
   for($i = 0; $i < count($material_name); $i++){
     if(!empty($_FILES['material_image']['name'])){
       $extension = pathinfo($_FILES['material_image']['name'][$i], PATHINFO_EXTENSION);
 
-      $_FILES['userFile']['name'] = $profile_name.'_'.$material_name[$i].'_'.$material_ID[$i].'/'.$material_OD[$i].'.'.$extension;
+      $_FILES['userFile']['name'] = $profile_name.'_'.$material_name[$i].'_'.$material_ID[$i].'-'.$material_OD[$i].'.'.$extension;
       $_FILES['userFile']['type'] = $_FILES['material_image']['type'][$i];
       $_FILES['userFile']['tmp_name'] = $_FILES['material_image']['tmp_name'][$i];
       $_FILES['userFile']['error'] = $_FILES['material_image']['error'][$i];
       $_FILES['userFile']['size'] = $_FILES['material_image']['size'][$i];
 
-      $uploadPath ='images/desktop/';  //upload images in upload/ folder
+      $uploadPath ='images/desktop/';  //upload images in images/desktop/ folder
       $config['upload_path'] = $uploadPath;
-          $config['allowed_types'] = 'gif|jpg|png|jpeg'; //allowed types of images           
+      $config['allowed_types'] = 'gif|jpg|png|jpeg'; //allowed types of images           
+      $config['overwrite'] = TRUE;            
 
       $this->load->library('upload', $config);  //load upload file config.
       $this->upload->initialize($config);
 
       if($this->upload->do_upload('userFile')){
         $fileData = $this->upload->data();
-        $uploadData[$i]['file_name'] = $fileData['file_name'];
-        $uploadData[$i]['created'] = date("Y-m-d H:i:s");
-        $uploadData[$i]['modified'] = date("Y-m-d H:i:s");
         $imagePath='images/desktop/'.$fileData['file_name'];
       }
     }
@@ -107,9 +135,10 @@ public function addProfile() {
   }
 
   $data['material_associated']=json_encode($material_Arr);
-
+  $data['profile_image']=($profileImg_path);
+  print_r($data);die();
   $path = base_url();                                                   // this code is for web service AND api for save profile 
-  $url = $path . 'api/ManageCustomer_api/save_CustomerDetails';
+  $url = $path . 'api/ManageProfile_api/save_Profile';
   $ch = curl_init($url);
   curl_setopt($ch, CURLOPT_POST, true);
   curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
