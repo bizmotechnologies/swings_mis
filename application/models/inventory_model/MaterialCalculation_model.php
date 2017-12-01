f3b6cdc56869ac8488d6ffc2d88fa4cf8323794b
swapnilbirajdar09 <swapnil.bizmotech@gmail.com>
2017-12-01 10:05:35 +0530

    commit by: swapnil

---
+++ application/models/inventory_model/MaterialCalculation_model.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
+class MaterialCalculation_model extends CI_Model {
+
+    public function GetRawMaterialBasePriceForEnquiry($material_id, $Material_ID, $Material_OD, $Material_LENGTH) {
+        
+    }
+
+}
+
+?>
